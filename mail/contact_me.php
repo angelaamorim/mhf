85f8ab4222d30c2eaf2db28cc6cea1af6f8a81db
angelaamorim <angelatca3@gmail.com>
2017-11-23 00:24:23 -0200

    Add files via upload

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,7 @@
 <?php
 // Check for empty fields
+
+
 if(empty($_POST['nome'])      ||
    empty($_POST['email'])     ||
    empty($_POST['telefone'])     ||
@@ -9,18 +11,18 @@ if(empty($_POST['nome'])      ||
    echo "No arguments Provided!";
    return false;
    }
-   
+
 $nome = strip_tags(htmlspecialchars($_POST['nome']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $telefone = strip_tags(htmlspecialchars($_POST['telefone']));
 $message = strip_tags(htmlspecialchars($_POST['mensagem']));
-   
+
 // Create the email and send the message
-$to = 'angelatca_3@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $nome\n\nEmail: $email_address\n\nPhone: $telefone\n\nMessage:\n$message";
-$headers = "From: angelatca3@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'mhf@mhf.com.br'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Contato:  $name";
+$email_body = "Você recebeu uma mensagem através do seu site\n\n"."Detalhes:\n\nNome: $nome\n\nEmail: $email_address\n\nTelefone: $telefone\n\nMessagem:\n$mensagem";
+$headers = "From: mhf@mhf.com.br\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;         
+return true;
 ?>
